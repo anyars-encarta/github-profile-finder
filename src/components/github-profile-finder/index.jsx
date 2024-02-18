f621a3850b9253211c57aed6e86790b309e5e7c6
anyars-encarta <anyarsencarta@gmail.com>
2024-02-18 07:26:50 +0000

    Complete Github Profile Search

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,21 +1,29 @@
 import { useState, useEffect } from "react";
 import UserCard from "./user-card";
+import './styles.css';
 
 const GitHubProfileFinder = () => {
     const [userName, setUserName] = useState('anyars-encarta');
     const [userData, setUserData] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const fetchGitHubUserData = async () => {
-      setLoading(true);
-      const response = await fetch(`https://api.github.com/users/${userName}`)
-      const data = await response.json();
+        try {
+            setLoading(true);
+            const response = await fetch(`https://api.github.com/users/${userName}`)
+            const data = await response.json();
 
-      if (data) {
-        setUserData(data);
-        setLoading(false);
-        setUserName('');
-      }
+            if (data) {
+                setUserData(data);
+                setLoading(false);
+                setUserName('');
+            }
+
+        } catch (e) {
+          setErrorMessage(e.message)
+          setLoading(false);
+        }
     };
 
     const handleSubmit = () => {
@@ -23,12 +31,21 @@ const GitHubProfileFinder = () => {
     };
 
     useEffect(() => {
-      fetchGitHubUserData();
+        fetchGitHubUserData();
+        //eslint-disable-next-line
     }, []);
 
 
     if (loading) {
-       return <h1>Loading data. Please wait...</h1>
+        return <h1>Loading data. Please wait...</h1>
+    };
+
+    if (errorMessage !== null) {
+        return (
+        <div>
+          <h1>There was an error: {errorMessage}</h1>
+        </div>
+        )
     };
 
     return (
--- src/components/github-profile-finder/user-card.jsx
+++ src/components/github-profile-finder/user-card.jsx
@@ -1,41 +1,43 @@
 const UserCard = ({ user }) => {
-    const { avatar_url, followers, following, login, name, public_repos, html_url, created_at} = user
+    const { avatar_url, followers, following, login, name, bio, public_repos, html_url, created_at, location } = user
 
     const createdDate = new Date(created_at);
 
     return (
         <div className="user">
-          <div>
-            <img src={avatar_url} className="avatar" alt="User" />
-          </div>
+            <div>
+                <img src={avatar_url} className="avatar" alt="User" />
+            </div>
 
-          <div>
-            <a href={html_url} target="_blank">{name || login}</a>
-            <p>User joined on: {' '}
-                {
-                  `${createdDate.getDate()} 
-                  ${createdDate.toLocaleString('en-us', {month: 'short'})}
+            <p><b>Bio: {' '}</b>{bio}</p>
+            <p><b>Address: {' '}</b>{location}</p>
+            <div className="name-container">
+                <a href={html_url} target="_blank" rel="noreferrer">{name || login}</a>
+                <p>User joined on: {' '}
+                    {
+                        `${createdDate.getDate()} 
+                  ${createdDate.toLocaleString('en-us', { month: 'short' })}
                   ${createdDate.getFullYear()}`
-                }
-            </p>
-          </div>
-
-          <div>
-            <div>
-              <p>Public Repos:</p>
-              <p>{public_repos}</p>
+                    }
+                </p>
             </div>
 
-            <div>
-              <p>Followers:</p>
-              <p>{followers}</p>
-            </div>
+            <div className="profile-info">
+                <div>
+                    <p>Public Repos:</p>
+                    <p>{public_repos}</p>
+                </div>
 
-            <div>
-              <p>Following:</p>
-              <p>{following}</p>
+                <div>
+                    <p>Followers:</p>
+                    <p>{followers}</p>
+                </div>
+
+                <div>
+                    <p>Following:</p>
+                    <p>{following}</p>
+                </div>
             </div>
-          </div>
         </div>
     )
 };
