5d766f555d879ed335aed647e44865814726f315
anyars-encarta <anyarsencarta@gmail.com>
2024-02-18 07:48:06 +0000

    Install and correct eslint errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import GitHubProfileFinder from './components/github-profile-finder/index';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      {/* <header className="App-header"> */}
-        <GitHubProfileFinder />
-      {/* </header> */}
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    {/* <header className="App-header"> */}
+    <GitHubProfileFinder />
+    {/* </header> */}
+  </div>
+);
 
 export default App;
